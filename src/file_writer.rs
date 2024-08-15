f8f4573e990ffd35e8a2200f489d05a0ce5a2481
Frank Krick <frank.krick@gmail.com>
2024-08-15 17:12:03 -0400

    diagnostic fixes

--- src/file_writer.rs
+++ src/file_writer.rs
@@ -9,7 +9,6 @@ pub async fn run_file_writer(
     loop {
         let data = receiver.recv().await;
         if receiver.is_empty() {
-            print!("{path:#?}");
             let data = serde_json::to_string_pretty(&data.unwrap()).unwrap();
             let mut file = File::create(path).await.unwrap();
             file.write_all(data.as_bytes()).await.unwrap();
